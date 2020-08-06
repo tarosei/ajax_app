93e07a9869d27a06c8b1cbffad4d65765ee9b8dd
tarosei <66665784+tarosei@users.noreply.github.com>
2020-08-06 11:36:16 +0900

    Revert "既読機能のサーバーサイド実装"
    This reverts commit 678e701460c1bc123c1467631f6b7dff43f0ddbc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  root to: 'posts#index'
-  post 'posts', to: 'posts#create'
-  get 'posts', to: 'posts#checked'
+  root :'post#index'
 end
--- db/migrate/20200712044343_ tweet_posts.rb
+++ db/migrate/20200712044343_ tweet_posts.rb
@@ -1,8 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.text :content
-      t.boolean :checked
+      t.string :name
+      t.string :text
+      t.text :image
       t.timestamps
     end
   end
